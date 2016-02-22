17918bcc9887717c2b7caff56ea360d19a27dca4
sattfatt <sattfatt@gmail.com>
2016-02-21 17:53:17 -0800

    Roughly finished GPS code

--- flight/gps/gps.ino
+++ flight/gps/gps.ino
@@ -1,11 +1,12 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(10, 11); // RX, TX
+SoftwareSerial GPSSerial(10, 11); // RX, TX
 
 char GPSbuffer[82];
 int i;
 int commas[10];
 int j;
+float GPSvals[6];
 
 void setup() {
   i = 0;//index
@@ -14,7 +15,7 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-  mySerial.begin(9600);
+  GPSSerial.begin(9600);
 }  
   
 
@@ -22,4 +23,26 @@ void loop() {
 
 }
 
+void getGPS()
+{
+  if (GPSSerial.available())
+  {
+    GPSSerial.readBytesUntil('\n', GPSbuffer, 82);
+  
+  
+    if( GPSbuffer[0]=='$' && GPSbuffer[4]=='M' && GPSbuffer[5]=='C')
+    {
+      GPSvals[0] = atof(GPSbuffer + commas[0]+1); //Time
+      GPSvals[1] = atof(GPSbuffer + commas[2]+1); //Latitude
+      GPSvals[2] = atof(GPSbuffer + commas[4]+1); //Longitude
+      GPSvals[3] = atof(GPSbuffer + commas[6]+1); //Knots
+    }
+
+    else if(GPSbuffer[0]=='$' && GPSbuffer[4]=='G' && GPSbuffer[5]=='A')
+    {
+      GPSvals[4] = atof(GPSbuffer + commas[6]+1); //SatNum
+      GPSvals[5] = atof(GPSbuffer + commas[8]+1); //Altitude
+    }    
+  }
+}
 
