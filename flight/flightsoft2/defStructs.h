50d5e5678271af629118aaad1a137f1561a4788b
sattfatt <sattfatt@gmail.com>
2016-05-24 12:33:28 -0700

    replaced header file for flightsoft2

---
+++ flight/flightsoft2/defStructs.h
@@ -0,0 +1,29 @@
+#ifndef flightsoft_h
+#define flightsoft_h
+
+typedef struct Packet {
+  //unsigned long time;
+  float pressure;
+  float altitude;
+  float temp;
+  float voltage;
+  float airspeed;
+  //float latitude;
+  //float longitude;
+  //float gpsalt;
+  //float satnum;
+  //float gpsspeed;
+//  unsigned long imgcmdTime;
+//  int imgcmdCount;
+  int bonus;
+} Packet_t;
+
+typedef struct Avg {
+  //unsigned long time;
+  float pressure;
+  float altitude;
+  float temp;
+  float airspeed;
+} Avg_t;
+
+#endif
